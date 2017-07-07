e3786a2346fde153c05e15871588b74a1e0b41fb
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2017-07-07 17:16:49 +0530

    fixed sorting issue

--- src/frontend/src/app/pipes/keys.ts
+++ src/frontend/src/app/pipes/keys.ts
@@ -8,6 +8,15 @@ export class KeysPipe implements PipeTransform
         for (let key in value) {
             keys.push({key: key, value: value[key]});
         }
+        keys.sort((a: any, b: any) => {
+        if (a.key < b.key) {
+            return -1;
+        } else if (a.key > b.key) {
+            return 1;
+        } else {
+            return 0;
+        }
+        }); 
         return keys;
     }
 }
