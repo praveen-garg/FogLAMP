b946832ff3405801ea6c56b9b015986ed3517408
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2017-08-04 21:15:24 +0530

    Added limit logs

--- src/frontend/src/app/audit-log/audit-log.component.ts
+++ src/frontend/src/app/audit-log/audit-log.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StatisticsService } from '../services/index';
 
-import * as _ from 'lodash';    
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-audit-log',
@@ -9,25 +9,35 @@ import * as _ from 'lodash';
   styleUrls: ['./audit-log.component.css']
 })
 export class AuditLogComponent implements OnInit {
-public auditLogs = []
-public audit_source = [];
-  constructor(private statisticsService : StatisticsService) { }
+  public auditLogs = []
+  public audit_source = [];
+  public audit_severity = [];
+  public range: Number = 0;
+  constructor(private statisticsService: StatisticsService) { }
 
   ngOnInit() {
-    this.getAuditLogs()
+    this.getAuditLogs(0)
   }
 
- public getAuditLogs(): void {
-   this.auditLogs = []
-    this.statisticsService.getAuditLogs().
+  public getAuditLogs(limit: Number = 0): void {
+    if (limit == null) {
+      return;
+    }
+    this.range = limit;
+    console.log("Limit: ", this.range);
+    this.auditLogs = []
+    this.statisticsService.getAuditLogs(limit).
       subscribe(
       data => {
         this.auditLogs = data.audit
-        this.audit_source = this.auditLogs.map(function(a) {return a.source;});
+
+        this.audit_source = this.auditLogs.map(function (a) { return a.source; });
         this.audit_source = _.uniq(this.audit_source)
-        console.log("Source", this.audit_source)
+
+        this.audit_severity = this.auditLogs.map(function (a) { return a.severity; });
+        this.audit_severity = _.uniq(this.audit_severity)
+        console.log("Source", this.audit_severity)
       },
       error => { console.log("error", error) })
   }
-
 }
--- src/frontend/src/app/services/statistics.service.ts
+++ src/frontend/src/app/services/statistics.service.ts
@@ -68,12 +68,12 @@ export class StatisticsService {
 /**
    *  GET | /foglamp/audit  
    */
-  public getAuditLogs() {
-    return this.http.get(this.GET_AUDIT_LOGS)
+  public getAuditLogs(limit:Number = 0) {
+    let params: URLSearchParams = new URLSearchParams();
+    return this.http.get(this.GET_AUDIT_LOGS, {params: {limit: limit}})
       .map(response => response.json())
       .catch((error: Response) => Observable.throw(error.json().message || 'Server error'))
   }
-
 
 
 }
