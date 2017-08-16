122767684b18510d2f2bf81c3ca32bb1f9114910
Praveen Garg <praveen@scaledb.com>
2017-08-17 01:30:23 +0530

    local hots url

--- src/frontend/src/app/services/utils.ts
+++ src/frontend/src/app/services/utils.ts
@@ -1,9 +1,9 @@
-//export const BASE_URL = 'http://0.0.0.0:8082/foglamp/'
-export const BASE_URL = 'http://192.168.1.10:8082/foglamp/'
+export const BASE_URL = 'http://0.0.0.0:8082/foglamp/'
+// export const BASE_URL = 'http://192.168.1.10:8082/foglamp/'
 export const POLLING_INTERVAL = 2000   //milliseconds
 
 export default class Utils {
-  
+
   public static formateDate(dt: string) {
     var date = new Date(dt); // had to remove the colon (:) after the T in order to make it work
     // var day = date.getDate();
