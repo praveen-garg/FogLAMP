bb9ffb72cfe5a605fac9f58776d5a5da63c481c0
Praveen <praveen.garg@nerdapplabs.com>
2017-08-18 04:26:10 +0530

    stats chart; 1 per row; show 20 delta points; display hh:mm:ss with padding

--- src/frontend/src/app/services/utils.ts
+++ src/frontend/src/app/services/utils.ts
@@ -1,5 +1,5 @@
-//export const BASE_URL = 'http://0.0.0.0:8082/foglamp/'
-export const BASE_URL = 'http://192.168.1.10:8082/foglamp/'
+export const BASE_URL = 'http://0.0.0.0:8082/foglamp/'
+// export const BASE_URL = 'http://192.168.1.10:8082/foglamp/'
 export const POLLING_INTERVAL = 2000   //milliseconds
 
 export default class Utils {
@@ -12,7 +12,12 @@ export default class Utils {
     var minutes = date.getMinutes();
     var hours = date.getHours();
     var seconds = date.getSeconds();
-    var myFormattedDate = hours + ":" + minutes;
+    var myFormattedDate = Utils.pad(hours,2,0) + ":" + Utils.pad(minutes,2,0) + ":" + Utils.pad(seconds,2,0);
     return myFormattedDate;
+  }
+  public static pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
   }
 }
