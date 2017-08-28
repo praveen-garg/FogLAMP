53972ce4477a28c3ad5cd1716182fbf723499fb1
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2017-08-28 17:46:43 +0530

    binding of source and severity keys dynamically

--- src/frontend/src/app/audit-log/audit-log.component.ts
+++ src/frontend/src/app/audit-log/audit-log.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AuditService } from '../services/index';
 
-import * as _ from 'lodash';
-
-enum SeverityEnum {
-  FATAL = 1,
-  ERROR = 2,
-  WARNING = 3,
-  INFORMATION = 4
-}
-
 @Component({
   selector: 'app-audit-log',
   templateUrl: './audit-log.component.html',
   styleUrls: ['./audit-log.component.css']
 })
 export class AuditLogComponent implements OnInit {
-  public sourceOptionsList = []
-  public severityLevelsList = []
+  public logSourceList = []
+  public logSeverityList = []
   public filterdData = []
+
   public limit: Number = 0
   public offset: Number = 0
   public source: String = ""
@@ -27,15 +19,29 @@ export class AuditLogComponent implements OnInit {
   constructor(private auditService: AuditService) { }
 
   ngOnInit() {
+    this.getLogSource()
+    this.getLogSeverity()
     this.getAuditLogs()
-    // Add SeverityEnum to severityLevelsList
-    // TODO: Severity keys must be coming from service
-    for (let item in SeverityEnum) {
-      if (isNaN(Number(item))) {
-        console.log(item);
-        this.severityLevelsList.push(item)
-      }
-    }
+  }
+
+  public getLogSource() {
+    this.auditService.getLogSource().
+      subscribe(
+      data => {
+        this.logSourceList = data.log_code
+        console.log("Log code", this.logSourceList);
+      },
+      error => { console.log("error", error) })
+  }
+
+  public getLogSeverity() {
+    this.auditService.getLogSeverity().
+      subscribe(
+      data => {
+        this.logSeverityList = data.log_severity
+        console.log("Log severity ", this.logSeverityList);
+      },
+      error => { console.log("error", error) })
   }
 
   public setLimit(limit: Number) {
@@ -46,7 +52,7 @@ export class AuditLogComponent implements OnInit {
 
   public setOffset(offset: Number) {
     this.offset = offset
-    console.log("offset: ", offset)
+    console.log("offset:", offset)
     if (this.limit != null) {
       this.getAuditLogs()
     }
@@ -79,9 +85,6 @@ export class AuditLogComponent implements OnInit {
       subscribe(
       data => {
         this.filterdData = data.audit
-        // TODO: source must come from service to know actual superset of sources
-        this.sourceOptionsList = _.uniq(this.filterdData.map(function (a) { return a.source }))
-        //this.severityLevelsList = _.uniq(this.auditLogs.map(function (a) { return a.severity }))
       },
       error => { console.log("error", error) })
   }
