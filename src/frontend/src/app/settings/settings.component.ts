9faaaf3525eaef6a6eefbad07f223a2176dbe976
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2017-10-09 20:17:28 +0530

    Fixed per feedback

--- src/frontend/src/app/settings/settings.component.ts
+++ src/frontend/src/app/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-settings',
@@ -9,16 +10,25 @@ import { Router } from '@angular/router';
 })
 export class SettingsComponent implements OnInit {
   @Output() toggle: EventEmitter<any> = new EventEmitter();
+  endpoint = environment.BASE_URL.split(':');
+  protocol = this.endpoint[0];
+  host = this.endpoint[1].substr(2);
+  port = this.endpoint[2].substring(0, this.endpoint[2].indexOf('/'));
   constructor(private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    console.log("HOST", this.host);
+    console.log("PORT", this.port);
+    console.log("PROTOCOL", this.protocol);
+    
+  }
 
   public resetEndPoint() {
     const protocolField = <HTMLSelectElement>document.getElementById('protocol');
-    const ipField = <HTMLInputElement>document.getElementById('ip');
+    const hostField = <HTMLInputElement>document.getElementById('host');
     const portField = <HTMLInputElement>document.getElementById('port');
-    const ip_address = protocolField.value + '://' + ipField.value + ':' + portField.value + '/foglamp/';
-    localStorage.setItem('API_END_POINT', ip_address);
+    const endpoint = protocolField.value + '://' + hostField.value + ':' + portField.value + '/foglamp/';
+    localStorage.setItem('API_END_POINT', endpoint);
     console.log('End Point', localStorage.getItem('API_END_POINT'));
     this.router.navigate(['']);
     window.location.reload();
