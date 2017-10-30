9a7c0b65df2415bd8f100148b3c26b747a77df07
msharma <monika.sharma@nerdapplabs.com>
2017-10-30 17:29:03 +0530

    Added some e2e Tests

--- src/frontend/e2e/app.e2e-spec.ts
+++ src/frontend/e2e/app.e2e-spec.ts
@@ -7,8 +7,53 @@ describe('foglampapp App', () => {
     page = new FogLAMPPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('Should Display Nav Title', () => {
+    page.navigateToHome();
+    expect(page.getNavTitle()).toEqual('FogLAMP Management');
+  });
+
+  it('Should Display App Status', () => {
+    page.navigateToHome();
+    expect(page.getAppStatus()).toEqual('service down');
+  });
+
+  it('Should Display Config Titles', () => {
+    var ConfigTitles = [
+      'Sensors and Device Interface',
+      'Log Partitioning',
+      'Streaming',
+      'System Purge',
+      'CoAP Device',
+      'Scheduler configuration',
+      'Device server configuration',
+      'Configuration of the Sending Process',
+      'Configuration of OMF types',
+      'Purge the readings table',
+      'Configuration of OMF Translator plugin'
+    ];
+    page.navigateToConfig();
+    for (var ConfigTitle in ConfigTitles) {
+      expect(page.getConfigTitles()).toContain(ConfigTitles[ConfigTitle]);
+    }
+  });
+
+  it('Should Display Scheduled Tasks', () => {
+    page.navToScheduledTasks();
+    expect(page.getTitleSchedules()).toEqual('Schedules');
+    expect(page.getTitleCreateSchedule()).toEqual('Create Schedule');
+    expect(page.getTitleTasks()).toContain('Tasks');
+  });
+
+  it('Should Display Audits Logs', () => {
+    page.navToAuditLogs();
+    expect(page.getAuditLogsTitle()).toEqual('Audit Logs');
+    expect(page.getSelectTag()).toEqual(2);
+    expect(page.getInputTag()).toEqual(2);
+  });
+
+  it('Should Display Assets & Readings', () => {
+    page.navToAssetReadings();
+    expect(page.getAssetsTitle()).toEqual('Assets');
+    expect(page.getAssetReadingsTitle()).toEqual('Asset Readings');
   });
 });
--- src/frontend/e2e/app.po.ts
+++ src/frontend/e2e/app.po.ts
@@ -1,11 +1,78 @@
 import { browser, by, element } from 'protractor';
 
 export class FogLAMPPage {
-  navigateTo() {
+  navigateToHome() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  navigateToConfig() {
+    return browser.get('/configuration');
+  }
+
+  navToScheduledTasks() {
+    return browser.get('/scheduled-tasks');
+  }
+
+  navToAuditLogs() {
+    return browser.get('/audit');
+  }
+
+  navToAssetReadings() {
+    return browser.get('/assets');
+  }
+
+  getNavTitle() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('.container .nav-item b')).getText();
+  }
+
+  getAppStatus() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('#app .nav-right span')).getText();
+  }
+
+  getConfigTitles() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('#app app-configuration-manager')).getText();
+  }
+
+  getTitleSchedules() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('app-scheduled-process .is-parent > div > header p')).getText();
+  }
+
+  getTitleCreateSchedule() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('app-scheduled-process app-create-schedule header p')).getText();
+  }
+
+  getTitleTasks() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('app-scheduled-process app-list-tasks header > div')).getText();
+  }
+
+  getAuditLogsTitle() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('app-audit-log nav small')).getText();
+  }
+
+  getSelectTag() {
+    browser.ignoreSynchronization = true;
+    return element.all(by.css('app-audit-log div:nth-child(1) select')).count();
+  }
+
+  getInputTag() {
+    browser.ignoreSynchronization = true;
+    return element.all(by.css('app-audit-log input')).count();
+  }
+
+  getAssetsTitle() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('app-assets div:nth-child(1) > div > article > h5 > small')).getText();
+  }
+
+  getAssetReadingsTitle() {
+    browser.ignoreSynchronization = true;
+    return element(by.css('app-assets div:nth-child(2) > div > article > h5 > small')).getText();
   }
 }
