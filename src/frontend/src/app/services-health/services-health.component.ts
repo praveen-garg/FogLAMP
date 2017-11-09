f2ad0e0925c6a89772f88d32b72eeaf57791f0be
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2017-11-09 22:35:50 +0530

    Fixed for management port issue

--- src/frontend/src/app/services-health/services-health.component.ts
+++ src/frontend/src/app/services-health/services-health.component.ts
@@ -11,8 +11,6 @@ import { environment } from '../../environments/environment';
 export class ServicesHealthComponent implements OnInit {
   public timer: any = '';
   public service_data;
-  // public ping_data = {};
-  // public ping_info = { is_alive: false, service_status: 'service down' };
   constructor(private servicesHealthService: ServicesHealthService) { }
 
   ngOnInit() {
@@ -20,41 +18,13 @@ export class ServicesHealthComponent implements OnInit {
   }
 
   public getServiceData() {
-    this.servicesHealthService.getServicesData()
+    this.servicesHealthService.getAllServices()
       .subscribe(
       (data) => {
-        console.log('Service Status', data.services);
         this.service_data = data.services;
       },
       (error) => {
         console.log('error: ', error);
-      }
-      );
-  }
-
-  // TODO: FOGL-516
-  checkServiceHealth(port) {
-    console.log('Port:', port);
-    /*
-    const service = this.service_data.filter(service => service.management_port == port);
-
-    if (service[0].address === 'localhost') {
-      service[0].address = environment.BASE_URL.substring(environment.BASE_URL.indexOf('/') + 2, environment.BASE_URL.lastIndexOf(':'));
-    }
-
-    const URL = service[0].protocol + '://' + service[0].address + ':' + service[0].management_port + '/';
-    this.servicesHealthService.checkServiceHealth(URL)
-      .subscribe(
-      (data) => {
-        this.ping_data = data;
-        this.ping_info = { is_alive: true, service_status: 'running...' };
-      },
-      (error) => {
-        console.log('error: ', error);
-        this.ping_info = { is_alive: false, service_status: 'service down' };
-      },
-      () => console.log(this.ping_info)
-    );
-    */
+      });
   }
 }
--- src/frontend/src/app/settings/settings.component.ts
+++ src/frontend/src/app/settings/settings.component.ts
@@ -14,17 +14,31 @@ export class SettingsComponent implements OnInit {
   protocol = this.endpoint[0];
   host = this.endpoint[1].substr(2);
   port = this.endpoint[2].substring(0, this.endpoint[2].indexOf('/'));
+  management_port = 0;
   constructor(private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    if (environment.MANAGEMENT_URL !== '') {
+      const url_items = environment.MANAGEMENT_URL.split(':');
+      console.log(url_items);
+      this.management_port = +url_items[2].substring(0, url_items[2].indexOf('/'));
+    }
+  }
 
   public resetEndPoint() {
     const protocolField = <HTMLSelectElement>document.getElementById('protocol');
     const hostField = <HTMLInputElement>document.getElementById('host');
     const portField = <HTMLInputElement>document.getElementById('port');
+    const managementPortField = <HTMLInputElement>document.getElementById('management_port');
     const endpoint = protocolField.value + '://' + hostField.value + ':' + portField.value + '/foglamp/';
-    localStorage.setItem('API_END_POINT', endpoint);
-    location.reload()
+    localStorage.setItem('SERVICE_URL', endpoint);
+
+    if (managementPortField.value !== '') {
+      // TODO make sure its a positive integer too
+      const management_endpoint = protocolField.value + '://' + hostField.value + ':' + managementPortField.value + '/foglamp/';
+      localStorage.setItem('MANAGEMENT_URL', management_endpoint);
+    }
+    location.reload();
     location.href = '';
     this.router.navigate([location.href]);
   }
