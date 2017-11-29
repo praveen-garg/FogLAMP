2a1686cb93292819d83395a0b78c236924702375
msharma <monika.sharma@nerdapplabs.com>
2017-11-29 14:17:33 +0530

    Fixed timestamp issue for Safari

---
+++ src/frontend/src/app/pipes/moment.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import * as moment from 'moment';
+
+/*
+ * Time helper using momentjs
+ * Usage:
+ *   timestamp | moment:'DD.MM.YYYY'
+ * Defaults to 'L' - locale ie. '01/24/2017'
+*/
+@Pipe({name: 'moment'})
+export class MomentPipe implements PipeTransform {
+  transform(value: string, arg: string): string {
+     return moment(value).format(arg);
+  }
+}
